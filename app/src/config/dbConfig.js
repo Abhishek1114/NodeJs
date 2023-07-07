7e3531f14f9433978d3e23ffa282f91147e2beac
Abhishek1114 <138587969+Abhishek1114@users.noreply.github.com>
2023-07-07 21:00:24 +0530

    Update dbConfig.js

--- app/src/config/dbConfig.js
+++ app/src/config/dbConfig.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 const {MONGO_URI} = process.env 
 
 module.exports.getDbConnection = function(){ 
-    mongoose.connect("mongodb://127.0.0.1:27017/mean23").then(()=>console.log("DbConnected")).
+    mongoose.connect("mongodb+srv://shahabhi1411:Abhi0273@cluster0.3drxg0m.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("DbConnected")).
     catch((err)=>console.log(err))
 }
