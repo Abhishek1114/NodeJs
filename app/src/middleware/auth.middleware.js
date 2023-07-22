123d942f9f394102687c83ac02e837460812e997
Abhishek1114 <138587969+Abhishek1114@users.noreply.github.com>
2023-07-22 13:30:24 +0530

    Update auth.middleware.js

--- app/src/middleware/auth.middleware.js
+++ app/src/middleware/auth.middleware.js
@@ -11,6 +11,14 @@ module.exports = function(req,res,next){
             res.json({msg:"Please Login before acccess the service",rcode:-9,data:""})
         }else{
             console.log("decoded => ",decoded);
+            const exptimestamp = decoded.exp
+            const currentTimeStamp = Math.floor(Date.now() / 1000);
+            const difference = exptimestamp - currentTimeStamp;
+            console.log("seconds:",difference)
+            if(difference<=600){
+              token = jwt.sign({"email":decoded.email,"userId":decoded._id,"role":"user"},'private',{expiresIn:"20m"})
+              res.json({msg:"This is your new token",rcode:200,token:token})  
+            }
             next();
         }
       })
