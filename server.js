d2e033af91b54313dfddaf45f56e421716773644
Abhishek1114 <138587969+Abhishek1114@users.noreply.github.com>
2023-10-12 19:57:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 require("./app/src/config/dbConfig").getDbConnection()
-
+var cors = require('cors')
 const categoryRoutes = require("./app/src/routes/category.routes")
 const productRoutes = require("./app/src/routes/product.routes")
 const publicRoutes = require("./app/src/routes/public.routes")
@@ -11,6 +11,7 @@ const app = express()
 //middlerware 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 
 //private ->authenticated 
 app.use("/admin",authMiddlerware,categoryRoutes)
